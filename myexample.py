fc3b61f770dee23704543a3af1bc13e411552131
kingfear42 <121379941+kingfear42@users.noreply.github.com>
2026-06-30 20:54:49 +0300

    Add files via upload

---
+++ myexample.py
@@ -0,0 +1,77 @@
+#  def toplama(a, b):
+#      sonuc = a + b
+#      return sonuc
+#  x = float(input("Birinci Sayıyı Giriniz:"))
+#  y = float(input("İkinci Sayıyı Giriniz:"))
+#  toplam = toplama(x, y)
+#  print(toplam)
+#  x = 5.0
+#  xtip = type(x)
+#  print(xtip)
+#  a = 4
+#  A = "Ağlama hadi oyna"
+#  print(a)
+#  print(A)
+#  print(type(a))
+#  print(type(A))
+#  myVariableName = "Camel Case"
+#  MyVariableName = "Pascal Case"
+#  my_variable_name = "Snake Case"
+#  my__variable__name = "Deneme2"
+#  print(myVariableName)
+#  print(MyVariableName)
+#  print(my_variable_name)
+#  print(my__variable__name)
+#  teams = ["Konyaspor", "Galatasaray", "Real Madrid"]
+#  x, y, z = teams
+#  print(x)
+#  print(y)
+#  print(z)
+#  x = 5
+#  y = 10
+#  print(x + y)
+#  a = 5
+#  b = "John"
+#  print(a, b)
+#  x = "awesome"
+#  def myfunc():
+#    x = "fantastic"
+#    print("Python is " + x)
+#  myfunc()
+#  print("Python is " + x)
+#  def myfunc():
+#    global x
+#    x = "fantastic"
+#  myfunc()
+#  print("Python is " + x)
+#  x = 3+5j
+#  y = 5j
+#  z = -5j
+#  print(type(x))
+#  print(type(y))
+#  print(type(z))
+#  import random
+#  print(random.randrange(1, 10))
+#  print("Ocoyano fatto beyni \nöyle böyle işte")
+#  x = 33
+#  x = x & 3
+#  print(x)
+#  teams = ["Bayern Münih", "Manchester United", "Kopenhag", "Galatasaray"]
+#  print(teams)
+#  print(len(teams))
+#  print(teams[0]) #Bayern Münih
+#  mixed = ["Konya", 42, True, 46, "Kahramanmaraş"]
+#  print(mixed)
+#  print(len(mixed))
+#  print(type(mixed))
+#  print(mixed[1]) #42
+#  thisislist = list(("Muhammed Ali Şen", "Muhammed Salih Orbay", "Taha Berk Küçük"))
+#  print(thisislist)
+#  print(type(thisislist))
+#  print(thisislist[2]) #Taha BErk KÜçük
+#  fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#  print(fruits[2:5])
+#  if "apple" in fruits:
+#      print("Yes, 'apple' is in the fruits list")
+
+
